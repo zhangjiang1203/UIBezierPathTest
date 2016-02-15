67a4544fb9ece4c2df23d67e200cd14291b658e2
zhangjiang <896884553@qq.com>
2016-02-15 13:23:07 +0800

    Initial Commit

---
+++ UIBezierPathTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIBezierPathTest
+//
+//  Created by zjhaha on 16/2/15.
+//  Copyright © 2016年 zjhaha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIBezierPathTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIBezierPathTest
+//
+//  Created by zjhaha on 16/2/15.
+//  Copyright © 2016年 zjhaha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
